831a3ee12c42045fd8c7089d0b1c04b867fca984
unknown <sivanidhi826@gmail.com>
2021-04-22 11:26:17 +0530

    first commit

---
+++ c#.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!"+"siva");
+        //Console.Write("Hello");
+        //Console.Write("Earth");
+        //Console.ReadLine();
+        
+        int a=5;
+        int b=9;
+        Console.WriteLine("Sum of "+a+"+"+b+"="+ (a+b));
+        Console.WriteLine("Sub of "+a+"+"+b+"="+ (a-b));
+        Console.WriteLine("Mul of "+a+"+"+b+"="+ (a*b));
+        Console.WriteLine("Div of "+a+"+"+b+"="+ (a/b));
+        Console.WriteLine("equality of "+a+"+"+b+"="+ (a==b));
+    }
+}
