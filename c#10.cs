7a373abe70799aa10b2be2b0435763027e97275f
unknown <sivanidhi826@gmail.com>
2021-04-22 18:03:21 +0530

    first commit

---
+++ c#10.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using System;
+public enum Salutation
+{
+Mr=0,
+Ms=1,
+Mrs=2,
+Dr=3
+}
+
+class 
+{
+ 
+    
+    static void Main()
+    {
+       Saluatation salutation
+string name;
+       Console.WriteLine("Enter the salutation:");
+salutation = (Salutation)Enum.Parse(typeof(Salutation), Console.ReadLine();
+//Console.WriteLine(typeof(string));
+Console.WriteLine("Enter your name:");
+Console.Write("\nYour Name is {0}" , salutation.ToString());
+Console.Write("," + name);
+}
+}
+}
